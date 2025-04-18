4720a90757788555570dabf4e565ab5eb3683c90
Andy Lietz <andy.lietz@stiftungpfadfinden.de>
2025-04-18 20:41:13 +0200

    Update extension framework to support Smarty 4

--- show_ratings_tab.php
+++ show_ratings_tab.php
@@ -1,10 +1,8 @@
 <?php
 
 require_once 'show_ratings_tab.civix.php';
-// phpcs:disable
-use CRM_ShowRatingsTab_ExtensionUtil as E;
 
-// phpcs:enable
+use CRM_ShowRatingsTab_ExtensionUtil as E;
 
 /**
  * Display a link to archive a case on the manage case screen
@@ -46,24 +44,6 @@ function show_ratings_tab_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function show_ratings_tab_civicrm_postInstall(): void {
-  _show_ratings_tab_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function show_ratings_tab_civicrm_uninstall(): void {
-  _show_ratings_tab_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
@@ -71,60 +51,3 @@ function show_ratings_tab_civicrm_uninstall(): void {
 function show_ratings_tab_civicrm_enable(): void {
   _show_ratings_tab_civix_civicrm_enable();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function show_ratings_tab_civicrm_disable(): void {
-  _show_ratings_tab_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function show_ratings_tab_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _show_ratings_tab_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function show_ratings_tab_civicrm_entityTypes(&$entityTypes): void {
-  _show_ratings_tab_civix_civicrm_entityTypes($entityTypes);
-}
-
-// --- Functions below this ship commented out. Uncomment as required. ---
-
-/**
- * Implements hook_civicrm_preProcess().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
- */
-//function show_ratings_tab_civicrm_preProcess($formName, &$form): void {
-//
-//}
-
-/**
- * Implements hook_civicrm_navigationMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
- */
-//function show_ratings_tab_civicrm_navigationMenu(&$menu): void {
-//  _show_ratings_tab_civix_insert_navigation_menu($menu, 'Mailings', [
-//    'label' => E::ts('New subliminal message'),
-//    'name' => 'mailing_subliminal_message',
-//    'url' => 'civicrm/mailing/subliminal',
-//    'permission' => 'access CiviMail',
-//    'operator' => 'OR',
-//    'separator' => 0,
-//  ]);
-//  _show_ratings_tab_civix_navigationMenu($menu);
-//}
